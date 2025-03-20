493aec2c04f312efc73ac67ac7ea874f1703a7bd
Noemi Baglieri <noemibaglieri@gmail.com>
2025-03-20 15:40:38 +0100

    adds 1 to 5 homework

--- D4.js
+++ D4.js
@@ -1,3 +1,5 @@
+const divider = "-----------------------";
+
 /* ESERCIZIO 1
  Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
 */
@@ -8,36 +10,87 @@ function area(l1, l2) {
 
 console.log(area(20, 20));
 
+console.log(divider);
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
  La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
  la loro somma moltiplicata per tre.
 */
 
-function crazySum(num1, num2) {}
+function crazySum(num1, num2) {
+  let sum = num1 + num2;
+  let multiSum = sum * 3;
 
+  if (num1 === num2) {
+    return multiSum;
+  } else {
+    return sum;
+  }
+}
+
+console.log("Il totale per due parametri uguali è: " + crazySum(3, 3));
+console.log("Il totale per due parametri diversi è: " + crazySum(2, 8));
+
+console.log(divider);
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function crazyDiff(num1) {
+  const num2 = 19;
+  let numDiff = num1 - num2;
 
+  if (num1 < num2) {
+    numDiff = num2 - num1;
+  }
+
+  if (num1 > num2) {
+    return console.log(numDiff * 3);
+  } else {
+    return console.log(numDiff);
+  }
+}
+
+crazyDiff(20);
+
+console.log(divider);
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function boundary(n) {
+  let a = (n >= 20 && n <= 100) || n === 400;
+  return console.log(a);
+}
 
+boundary(19);
+boundary(20);
+boundary(100);
+boundary(101);
+boundary(400);
+
+console.log(divider);
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
  La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
  ritornare la stringa originale senza alterarla.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function epify(string) {
+  let epicString = "EPICODE ";
 
+  if (string.startsWith(epicString)) {
+    return string;
+  } else if (string !== epicString) {
+    return epicString.concat(string);
+  }
+}
+
+console.log(epify("EPICODE cicciomiao"));
+
+console.log(divider);
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
